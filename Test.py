53272fd963df749c6eb48cf52f59e647fae0b388
mhjang <mhjang@plateer.com>
2026-06-26 10:32:29 +0900

    Update run all notion

---
+++ Test.py
@@ -0,0 +1,60 @@
+x =5
+if x > 2:
+    print ("yes")
+
+
+text = "hello"
+num = "12345"
+
+print (text[1]+num[3])
+
+
+
+print ("abc".replace("a","z"))
+
+x = 256
+y = 256
+print(x == y)
+
+
+text ="python"
+print (text * 3)
+
+
+a=[1,2]
+b=[1,2]
+
+print(a==b, a is b)
+
+print(-5 % 2)
+
+
+x=[1,2,3]
+print(sum(x))
+
+a=0.1+0.2
+print(abs(a - 0.3) < 1e-9)
+
+
+x=9
+print(x // 4)
+
+
+name = "python"
+print(name[::-1])
+
+
+def my_func():
+    print("hello")
+
+result = my_func()
+print(result)
+
+
+#tup = (1,2,3)
+#tup[0] = 9 
+#print(tup)
+
+a=2
+b=0
+print(a**b)
